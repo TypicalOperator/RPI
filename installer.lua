4f986907de3c9af0d71616ed561a14a1537912cc
TypicalOperator <81247022+TypicalOperator@users.noreply.github.com>
2022-09-29 23:51:17 +0300

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,7 +1,7 @@
 function RPI(method, object)
   if method == "install" or "Install" then
     if object == "LuaToJs" then
-      loadstring(game:HttpGet("raw.githubusercontent.com/TypicalOperator/RPI/maun/installations/LuaToJs.lua")
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/TypicalOperator/RPI/main/Installations/LuaToJs.lua")
     end
   end
 end
