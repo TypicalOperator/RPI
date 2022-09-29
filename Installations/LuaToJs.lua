ab880b582d32af2d3053285a55d33abd92ea7bbc
TypicalOperator <81247022+TypicalOperator@users.noreply.github.com>
2022-09-29 23:56:13 +0300

    Update LuaToJs.lua

--- Installations/LuaToJs.lua
+++ Installations/LuaToJs.lua
@@ -1,1 +1,15 @@
-
+const = _G
+local console = {}
+function console.log(string)
+  print(string)
+end
+function console.error(string)
+  error(string)
+end
+function console.warn(string)
+  warn(string)
+end
+function require(object)
+  return loadstring(game:HttpGet(object))()
+end
+-- more come soon!
