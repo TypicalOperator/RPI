e3bd6ab52f67d045c6896134a95cc87f67304a2d
TypicalOperator <81247022+TypicalOperator@users.noreply.github.com>
2022-09-29 23:49:47 +0300

    Create installer.lua

---
+++ installer.lua
@@ -0,0 +1,7 @@
+function RPI(method, object)
+  if method == "install" or "Install" then
+    if object == "LuaToJs" then
+      loadstring(game:HttpGet("raw.githubusercontent.com/TypicalOperator/RPI/maun/installations/LuaToJs.lua")
+    end
+  end
+end
